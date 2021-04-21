dbbf6336e56f8e9f7908862d84464c06595c70d4
adarshgoswami01 <adarshgoswami01@gmail.com>
2021-04-21 23:03:14 +0530

    added home

---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './home.css';
+
+class home extends Component{
+    render(){
+        return (
+            <div></div>
+        )
+
+        }
+    }
+
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import home from './screens/home/home';
 
 ReactDOM.render(
-  <div>
-    </div>,
+  <home />,
   document.getElementById('root')
 );
 
